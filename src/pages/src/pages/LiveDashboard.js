6eb7453d92014e751e665dea00f749018b248e8d
parv jain <parvjain123789@gmail.com>
2026-09-20 01:16:36 +0530

    Delete src/pages/src/pages directory

--- src/pages/src/pages/LiveDashboard.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const LiveDashboard = () => <h1>Live Dashboard</h1>;
-export default LiveDashboard;
--- src/pages/src/pages/src/pages/Analytics.js
+++
@@ -1,3 +0,0 @@
-import React from 'react';
-const Analytics = () => <h1>Analytics Page</h1>;
-export default Analytics;
